4e06fd288519e8ec1c04d1fb616463a6f89987c0
Lakssterius <lakssterius@gmail.com>
2024-04-12 19:03:58 +0300

    Фикс смертельных зон

--- Assets/Scripts/Triggers/Behaviours/DamageBehaviour.cs
+++ Assets/Scripts/Triggers/Behaviours/DamageBehaviour.cs
@@ -5,11 +5,16 @@ namespace Character.Abilities
     public class DamageBehaviour : CollisionBehaviour
     {
         [SerializeField] private int _damage = 1;
+        [SerializeField] private bool _permaDeath = false;
 
         public override void OnHit(Collider other)
         {
-            if (other.attachedRigidbody && other.attachedRigidbody.TryGetComponent<CharacterHealth>(out var health))
-                health.GetDamage(_damage);
+            if (other.attachedRigidbody && other.attachedRigidbody.TryGetComponent<CharacterHealth>(out var health)) {
+                if (!_permaDeath)
+                    health.GetDamage(_damage);
+                else
+                    health.PermaDeath();
+            }
         }
     }
 }

9d64735f18f3141f16203209927200bcb33305ce
Lakssterius <lakssterius@gmail.com>
2024-04-15 11:08:25 +0300

    Фикс двойного использования уклонения вверх/вниз

--- Assets/Scripts/Character/CharacterAnimation.cs
+++ Assets/Scripts/Character/CharacterAnimation.cs
@@ -6,6 +6,7 @@ namespace Character
     public class CharacterAnimation : MonoBehaviour
     {
         private Animator _animator;
+        private ActivityState _activityState;
 
         private void Awake()
         {
@@ -14,18 +15,49 @@ namespace Character
 
         public void Jump()
         {
+            if (!SetActivityState(ActivityState.Jump))
+                return; 
+            
             _animator.SetTrigger("jump");
         }
 
         public void Roll()
         {
+            if (!SetActivityState(ActivityState.Roll))
+                return;
+
             _animator.SetTrigger("roll");
         }
 
         public void Death()
         {
+            if (!SetActivityState(ActivityState.Death))
+                return;
+
             _animator.SetTrigger("death");
         }
+
+        public void ResetState()
+        {
+            _activityState = ActivityState.None;
+        }
+
+        private bool SetActivityState(ActivityState activityState)
+        {
+            if (_activityState == activityState)
+                return false;
+
+            _activityState = activityState;
+            return true;
+        }
+    }
+
+    public enum ActivityState
+    {
+        None,
+        Jump,
+        Roll,
+        Death
     }
 }
 

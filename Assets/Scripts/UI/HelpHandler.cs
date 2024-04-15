21480b96e156c474b8567d7273fba1a8b13c20ae
Lakssterius <lakssterius@gmail.com>
2024-04-15 10:27:41 +0300

    Добавил обучение

---
+++ Assets/Scripts/UI/HelpHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class HelpHandler : MonoBehaviour
+    {
+        [SerializeField] private Text _helpField;
+        [SerializeField, TextArea] private string _generalDescription;
+        [SerializeField, TextArea] private string _handheldDescription;
+        [SerializeField, TextArea] private string _desktopDescription;
+
+        private void Awake()
+        {
+            string helpText = _generalDescription;
+            if (SystemInfo.deviceType == DeviceType.Handheld)
+                helpText += _handheldDescription;
+            else
+                helpText += _desktopDescription;
+
+            _helpField.text = helpText;
+        }
+    }
+}
+
